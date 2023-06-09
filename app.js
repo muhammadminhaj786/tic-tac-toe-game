abb58e97dddba3e6c54c64d2197b8bef04930162
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-06-09 04:17:09 -0700

    almost

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var box7 = document.getElementById('box-7')
 var box8 = document.getElementById('box-8')
 var box9 = document.getElementById('box-9')
 
+var match ;
+
 var para = document.getElementById('para')
 //creating a flag 
 var flag = true
@@ -18,21 +20,60 @@ function Play(el){
     if(flag==true){
         //user first click flag value true
         el.innerHTML="X"
-        console.log(el)
+        // console.log(el)
         el.classList.add('txt')
         el.classList.add('disableel')
         //change flag value
         flag = false
     }else{
-        el.innerHTML="0"
+        el.innerHTML="O"
         el.classList.add('txt-zero')
         el.classList.add('disableel')
         flag=true
     }
-    
+    if ((box1.innerHTML=="O") && ( box2.innerHTML=="O") && (box3.innerHTML=="O")
+    || ((box1.innerHTML=="O") && (box4.innerHTML=="O") && (box7.innerHTML=="O"))
+    || ((box2.innerHTML=="O") && ( box5.innerHTML=="O") && (box8.innerHTML=="O")) 
+    || ((box3.innerHTML=="O") && ( box6.innerHTML=="O") && (box9.innerHTML=="O")) 
+    || ((box4.innerHTML=="O") && ( box5.innerHTML=="O") && (box6.innerHTML=="O"))
+    || ((box7.innerHTML=="O") && ( box8.innerHTML=="O") && (box9.innerHTML=="O"))
+    || ((box1.innerHTML=="O") && ( box5.innerHTML=="O") && (box9.innerHTML=="O"))
+    || ((box7.innerHTML=="O") && ( box5.innerHTML=="O") && (box3.innerHTML=="O"))) {
+        box6.classList.add('disableel')
+        box7.classList.add('disableel')
+        box8.classList.add('disableel')
+        box9.classList.add('disableel')
+        box5.classList.add('disableel')
+        box4.classList.add('disableel')
+        box3.classList.add('disableel')
+        box2.classList.add('disableel')
+        box1.classList.add('disableel')
+        console.log('O has won')
+        
+    }else if ((box1.innerHTML=="X") && ( box2.innerHTML=='X') && (box3.innerHTML=='X')
+    || ((box1.innerHTML=="X") && (box4.innerHTML=="X") && (box7.innerHTML=="X"))
+    || ((box2.innerHTML=="X") && ( box5.innerHTML=='X') && (box8.innerHTML=='X')) 
+    || ((box3.innerHTML=="X") && ( box6.innerHTML=='X') && (box9.innerHTML=='X')) 
+    || ((box4.innerHTML=="X") && ( box5.innerHTML=='X') && (box6.innerHTML=='X'))
+    || ((box7.innerHTML=="X") && ( box8.innerHTML=='X') && (box9.innerHTML=='X'))
+    || ((box1.innerHTML=="X") && ( box5.innerHTML=='X') && (box9.innerHTML=='X'))
+    || ((box7.innerHTML=="X") && ( box5.innerHTML=='X') && (box3.innerHTML=='X'))) {
+        box6.classList.add('disableel')
+        box7.classList.add('disableel')
+        box8.classList.add('disableel')
+        box9.classList.add('disableel')
+        box5.classList.add('disableel')
+        box4.classList.add('disableel')
+        box3.classList.add('disableel')
+        box2.classList.add('disableel')
+        box1.classList.add('disableel')
+        console.log('X has won')
+    }else{
+        match = "Tied"
+    }
 
 }
-
+console.log(match)
 
 //Creating a timer
 var mint = document.querySelector('.mint')
